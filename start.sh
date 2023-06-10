acb06fc70a4f22873a8a5dcb4de0a0beebe102f1
ctc948040 <mir948040@naver.com>
2023-06-10 12:08:02 +0900

    깃 업데이트 스크립트 추가

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 cd /efs/hsms/hsms-boot
+git pull origin
 kill -15 `ps -ef | grep hsms-boot-0.0.1-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
 sh ./mvnw clean package  -Dmaven.test.skip=true
 java -jar ./target/hsms-boot-0.0.1-SNAPSHOT.jar > hsms.log &
